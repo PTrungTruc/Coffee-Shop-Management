22d206165aaac256bfd347cfc018492799bb2b22
PTrungTruc <147294775+PTrungTruc@users.noreply.github.com>
2026-01-11 20:30:34 +0700

    Add files via upload

---
+++ jojo/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jojo.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // Action xử lý lỗi chung
+        public ActionResult Index()
+        {
+            ViewBag.ErrorMessage = "Có lỗi xảy ra trong ứng dụng.";
+            return View();
+        }
+
+        // Action xử lý lỗi HTTP
+        public ActionResult HandleError(int statusCode)
+        {
+            // Kiểm tra nếu có aspxerrorpath, chuyển hướng lại đúng trang lỗi
+            
+            ViewBag.ErrorMessage = statusCode == 404 ? "Không tìm thấy trang bạn yêu cầu." : "Có sự cố xảy ra.";
+            Response.StatusCode = statusCode;
+            return View("Index.cshtml");
+        }
+    }
+}
---
+++ jojo/DTO/AdminDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jojo.DTO
+{
+    public class AdminDto
+    {
+        public int id { get; set; }
+	    public string taikhoan { get; set; }
+        public string matkhau { get; set; }
+    }
+}
